65496da95de52e9e953e2087e2441ad20fef3c35
Heather Benton <heather.leigh4@gmail.com>
2021-03-26 18:56:51 -0400

    Changes to date founded, mascot, colors, and motto

--- mit.js
+++ mit.js
@@ -1,9 +1,5 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
     colors: ['Silver', 'Gray'],
     mascot: "engineers",
     founded: 1861,
